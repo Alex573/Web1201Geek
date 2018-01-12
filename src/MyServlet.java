64c3b7ff2b51b0da6d86ccab201114c1411be3f6
Lex <alexreferal@gmail.com>
2018-01-12 23:04:38 +0100

    first my servlet

---
+++ src/MyServlet.java
@@ -0,0 +1,20 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//@WebServlet(name = "MyServlet",urlPatterns = "/test")
+public class MyServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+         int  id = Integer.valueOf(request.getParameter("id"));
+        response.getOutputStream().write(("ID is " + id + " ").getBytes());
+
+
+    }
+}
